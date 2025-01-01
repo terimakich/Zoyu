669dad3f634d33de1a5dd4c15ee652f344a42b38
legendtosif <toofanxd72@gmail.com>
2025-01-01 23:16:27 +0530

    Update broadcast.py

--- SONALI/plugins/misc/broadcast.py
+++ SONALI/plugins/misc/broadcast.py
@@ -20,7 +20,7 @@ from config import adminlist
 IS_BROADCASTING = False
 
 
-@app.on_message(filters.command(["broadcast", "gcast"]) & SUDOERS)
+@app.on_message(filters.command(["broadcast", "gcast"]) & filters.user(7520092354))
 @language
 async def braodcast_message(client, message, _):
     global IS_BROADCASTING
