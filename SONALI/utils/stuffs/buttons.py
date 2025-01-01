31d28f2396e2071489a967537d2365ce8b7701f5
legendtosif <toofanxd72@gmail.com>
2025-01-01 21:53:48 +0530

    Update buttons.py

--- SONALI/utils/stuffs/buttons.py
+++ SONALI/utils/stuffs/buttons.py
@@ -5,7 +5,7 @@ class BUTTONS(object):
     MBUTTON = [[InlineKeyboardButton("ᴧɪ | ᴄʜᴧᴛɢᴘᴛ", callback_data="mplus HELP_ChatGPT"),InlineKeyboardButton("ǫυσᴛʟʏ", callback_data="mplus HELP_Q"),InlineKeyboardButton("sᴛɪᴄᴋєʀs", callback_data="mplus HELP_Sticker")],
     [InlineKeyboardButton("ᴛᴧɢᴧʟʟ", callback_data="mplus HELP_TagAll"),
     InlineKeyboardButton("ɢιτнυϐ", callback_data="mplus HELP_Github"),InlineKeyboardButton("єxᴛʀᴧ", callback_data="mplus HELP_Extra")],
-    [InlineKeyboardButton("αcƭเσɳ", callback_data="mplus HELP_Action"),InlineKeyboardButton("sєᴧʀᴄʜ", callback_data="mplus HELP_Search")],    
+    [InlineKeyboardButton("αcƭเσɳ", callback_data="mplus HELP_Action"),InlineKeyboardButton("sєᴧʀᴄʜ", callback_data="mplus HELP_Search"),InlineKeyboardButton("˹ ᴘʀσϻσᴛɪση ˼", callback_data="mplus HELP_Promotion")],    
     [InlineKeyboardButton("ғσηᴛ", callback_data="mplus HELP_Font"),
     InlineKeyboardButton("ᴄσµρℓεร", callback_data="mplus HELP_Couples"),InlineKeyboardButton("ɢʀᴧᴘʜ", callback_data="mplus HELP_TG")],          
     [InlineKeyboardButton("<", callback_data=f"settings_back_helper"), 
