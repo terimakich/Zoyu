59e7275eff2a847dd720789db97480eb784e2f15
legendtosif <toofanxd72@gmail.com>
2025-01-03 16:41:54 +0530

    Update love.py

--- SONALI/plugins/yumi/love.py
+++ SONALI/plugins/yumi/love.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 import random
-from SACHIN_MUSIC import app
+from SONALI import app
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
         
@@ -26,7 +26,7 @@ def get_random_message(love_percentage):
 
 EVAA = [
     [
-        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/SUCHI_MUSIC_BOT?startgroup=true"),
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/ZOYUMUSICBOT?startgroup=true"),
     ],
 ]
 
