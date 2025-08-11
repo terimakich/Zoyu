f7d296d9c11f9bbc162edab6555b99250ca6b743
terimakich <botxd495@gmail.com>
2025-08-11 11:02:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -51,8 +51,11 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
+API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
+API_KEY = getenv("API_KEY", '30DxNexGenBots6d066c')
+
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/The_incricible")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT","https://t.me/ZOYU_SUPPORT")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT","https://t.me/TEAMINCRICIBLE")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
