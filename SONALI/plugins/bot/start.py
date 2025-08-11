0465ac796f033698f78c9e1eda833cdaece84a65
terimakich <botxd495@gmail.com>
2025-08-11 11:55:56 +0530

    Update start.py

--- SONALI/plugins/bot/start.py
+++ SONALI/plugins/bot/start.py
@@ -29,25 +29,25 @@ from strings import get_string
 #--------------------------
 
 NEXI_VID = [
-    "https://files.catbox.moe/alq0pu.jpg",
-    "https://files.catbox.moe/alq0pu.jpg",
-    "https://files.catbox.moe/alq0pu.jpg",
-    "https://files.catbox.moe/xvsqrc.jpg",
-    "https://files.catbox.moe/xvsqrc.jpg",
-    "https://files.catbox.moe/alq0pu.jpg",
-    "https://files.catbox.moe/alq0pu.jpg",
-    "https://files.catbox.moe/alq0pu.jpg",
-    "https://files.catbox.moe/alq0pu.jpg",
-    "https://files.catbox.moe/alq0pu.jpg",
-    "https://files.catbox.moe/alq0pu.jpg",
-    "https://files.catbox.moe/alq0pu.jpg",
-    "https://files.catbox.moe/alq0pu.jpg",
-    "https://files.catbox.moe/alq0pu.jpg",
-    "https://files.catbox.moe/alq0pu.jpg",
-    "https://files.catbox.moe/alq0pu.jpg",
-    "https://files.catbox.moe/alq0pu.jpg",
-    "https://files.catbox.moe/alq0pu.jpg",
-    "https://files.catbox.moe/alq0pu.jpg",
+    "https://files.catbox.moe/4q7c4w.jpg",
+    "https://files.catbox.moe/90z6sq.jpg",
+    "https://files.catbox.moe/rdfi4z.jpg",
+    "https://files.catbox.moe/6f9rgp.jpg",
+    "https://files.catbox.moe/99wj12.jpg",
+    "https://files.catbox.moe/ezpnd2.jpg",
+    "https://files.catbox.moe/e7q55f.jpg",
+    "https://files.catbox.moe/qyfsi7.jpg",
+    "https://files.catbox.moe/kbke7s.jpg",
+    "https://files.catbox.moe/7icvpu.jpg",
+    "https://files.catbox.moe/4hd77z.jpg",
+    "https://files.catbox.moe/yn7wje.jpg",
+    "https://files.catbox.moe/kifsir.jpg",
+    "https://files.catbox.moe/zi21kc.jpg",
+    "https://files.catbox.moe/z0gh23.jpg",
+    "https://files.catbox.moe/f2s4ws.jpg",
+    "https://files.catbox.moe/26nzoq.jpg",
+    "https://files.catbox.moe/fu6jk3.jpg",
+    "https://telegra.ph/file/d30d11c4365c025c25e3e.jpg",
 
 ]
 
@@ -59,7 +59,7 @@ async def start_pm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            return await message.reply_video(
+            return await message.reply_photo(
                 random.choice(NEXI_VID),
                 caption=_["help_1"].format(config.SUPPORT_CHAT),
                 reply_markup=keyboard,
